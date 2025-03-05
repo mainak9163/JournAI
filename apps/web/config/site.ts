de560cede867cec15482aadeda929e8e213ecc4b
mainak <heellomeelo@gmail.com>
2025-03-05 11:55:42 +0530

    fix: github link fixed

--- apps/web/config/site.ts
+++ apps/web/config/site.ts
@@ -2,22 +2,14 @@ export const siteConfig = {
   name: "JournAI",
   url: "https://journai.vercel.app",
   ogImage: "https://magicui.design/og",
-  description:
-    "AI Journal App for smarter journaling.",
+  description: "AI Journal App for smarter journaling.",
   links: {
     // twitter: "https://twitter.com/magicuidesign",
     // discord: "https://discord.gg/87p2vpsat5",
-    github: "https://github.com/career-tokens/JournAI",
+    github: "https://github.com/mainak9163/JournAI",
     // instagram: "https://instagram.com/magicuidesign/",
   },
-  keywords: [
-    "Journal",
-    "AI Chat",
-    "Sentiment Analsysis",
-    "AI Summary",
-    "Sentiment Score",
-    "Open Source",
-  ],
+  keywords: ["Journal", "AI Chat", "Sentiment Analsysis", "AI Summary", "Sentiment Score", "Open Source"],
 };
 
 export type SiteConfig = typeof siteConfig;
